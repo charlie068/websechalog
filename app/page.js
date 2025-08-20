e7109d74548b800b9f714e50059898556e2ec336
Jean-Charles Isner <charlie068@gmail.com>
2025-08-20 08:15:08 +0100

    rm page.js

--- app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center" }}>
-      <h1>Hello World 👋</h1>
-    </main>
-  );
-}
