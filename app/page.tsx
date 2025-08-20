f76b0adfe248707c8a538fa09d2c39d7582fc155
Jean-Charles Isner <charlie068@gmail.com>
2025-08-20 08:20:39 +0100

    change layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 export default function Home() {
   return (
-    <main style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center" }}>
+    <main
+      style={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <h1>Hello World 👋</h1>
     </main>
   );
